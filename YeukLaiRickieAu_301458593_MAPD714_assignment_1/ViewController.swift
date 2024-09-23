b0fd920fc460fa45d97b796b989ed5850a8999e6
rickie <rcya996001@gmail.com>
2024-09-23 19:17:08 -0400

    Initial Commit

---
+++ YeukLaiRickieAu_301458593_MAPD714_assignment_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YeukLaiRickieAu_301458593_MAPD714_assignment_1
+//
+//  Created by Rickie Au on 23/9/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YeukLaiRickieAu_301458593_MAPD714_assignment_1Tests/YeukLaiRickieAu_301458593_MAPD714_assignment_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  YeukLaiRickieAu_301458593_MAPD714_assignment_1Tests.swift
+//  YeukLaiRickieAu_301458593_MAPD714_assignment_1Tests
+//
+//  Created by Rickie Au on 23/9/2024.
+//
+
+import Testing
+@testable import YeukLaiRickieAu_301458593_MAPD714_assignment_1
+
+struct YeukLaiRickieAu_301458593_MAPD714_assignment_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
